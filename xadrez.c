84e75adf9994ec5a6f59204b518b54cf4fa1539c
Francisco Honorato <123344918+FranciscoHonorat@users.noreply.github.com>
2025-02-03 18:53:25 +0000

    segundo commit

--- xadrez.c
+++ xadrez.c
@@ -5,78 +5,118 @@
 #define TORRE_MAX 5
 #define RAINHA_MAX 8
 
+void movimentacao_bispo(int bispo){
+    if(bispo < BISPO_MAX){
+        printf("O bispo %d casas na diganol superior direita\n", bispo);
+    }else{
+        printf("O bispo nao pode andar $d casas\n", bispo);
+        prinft("Digite o numero de casas que o dispo pode andar: ");
+        scanf("%d", bispo);
+        movimentacao_bispo(bispo);
+    }
+}
 
-void movimentacao(){
-    int bispo; //casas para a diagonal superior direita
-    int torre; //casas para a direita
-    int rainha; //casas para a esquerda
+void movimentacao_rainha(int rainha){
+        if(rainha < RAINHA_MAX){
+        printf("A Rainha %d casas na diganol superior direita\n", rainha);
+    }else{
+        printf("A rainha nao pode andar $d casas\n", rainha);
+        prinft("Digite o numero de casas que o dispo pode andar: ");
+        scanf("%d", rainha);
+        movimentacao_bispo(rainha);
+    }
+}
 
-    while(1){
-        printf("Digite o numero de casas que o bispo pode andar: ");
-        scanf("%d", &bispo);
-        if(bispo <= BISPO_MAX){
-            printf("O bispo andou %d casas na diagonal superior direita\n", bispo);
-            break;
-        }else if(bispo > BISPO_MAX){
-            printf("O bispo nao pode andar %d casas\n", bispo);
-        }else{
-            printf("valor invalido\n");
+void movimentacao_torre(int torre){
+        if(torre < TORRE_MAX){
+        printf("A torre %d casas na diganol superior direita\n", torre);
+    }else{
+        printf("A torre nao pode andar $d casas\n", torre);
+        prinft("Digite o numero de casas que o dispo pode andar: ");
+        scanf("%d", torre);
+        movimentacao_bispo(torre);
+    }
+}
+
+void movimentacao_cavalo(int cavalo){
+    for(int i = 0; i < 1; i++){
+        int j = 0;
+        while(j < 1){
+            printf("O cavalo se movimentou em L para cima e para direita\n");
+            j++;
         }
     }
+}
+
+void movimentacao(){
+    int bispo, torre, rainha, cavalo;
+
+    printf("Digite o numero de casas que o bispo pode andar: ");
+    scanf("%d", &bispo);
+    movimentacao_bispo(bispo);
+
+    printf("Digite o numero de casas que a rainha pode andar: ");
+    scanf("%d", &rainha);
+    movimentacao_rainha(rainha);
+
+    printf("Digite o numero de casas que o torre pode andar: ");
+    scanf("%d", &torre);
+    movimentacao_torre(torre);
+
+    printf("Digite o numero de casas que o torre pode andar: ");
+    scanf("%d", &cavalo);
+    movimentacao_cavalo(cavalo);
+
+    // while(1){
+    //     printf("Digite o numero de casas que o bispo pode andar: ");
+    //     scanf("%d", &bispo);
+    //     if(bispo <= BISPO_MAX){
+    //         printf("O bispo andou %d casas na diagonal superior direita\n", bispo);
+    //         break;
+    //     }else if(bispo > BISPO_MAX){
+    //         printf("O bispo nao pode andar %d casas\n", bispo);
+    //     }else{
+    //         printf("valor invalido\n");
+    //     }
+    // }
     
-    while(1){
-		printf("Digite o numero de casas que a rainha pode andar: ");
-        scanf("%d", &rainha);
-        if(rainha <= RAINHA_MAX){
-            printf("A rainha andou %d casas para a esquerda\n", rainha);
-            break;
-        }else if (rainha > RAINHA_MAX){
-            printf("A rainha nao pode andar %d casas\n", rainha);
-        }else{
-            printf("valor invalido\n");
-        }
-	}
+    // while(1){
+	// 	printf("Digite o numero de casas que a rainha pode andar: ");
+    //     scanf("%d", &rainha);
+    //     if(rainha <= RAINHA_MAX){
+    //         printf("A rainha andou %d casas para a esquerda\n", rainha);
+    //         break;
+    //     }else if (rainha > RAINHA_MAX){
+    //         printf("A rainha nao pode andar %d casas\n", rainha);
+    //     }else{
+    //         printf("valor invalido\n");
+    //     }
+	// }
 	
-	while(1){
-		printf("Digite o numero de casas que o torre pode andar: ");
-        scanf("%d", &torre);
-        if(torre <= TORRE_MAX){
-            printf("A torre andou %d casas na direita\n", torre);
-            break;
-        }else if (torre > TORRE_MAX){
-            printf("A torre nao pode andar %d casas\n", torre);
-        }else{
-            printf("valor invalido\n");
-        }
-	}
+	// while(1){
+	// 	printf("Digite o numero de casas que o torre pode andar: ");
+    //     scanf("%d", &torre);
+    //     if(torre <= TORRE_MAX){
+    //         printf("A torre andou %d casas na direita\n", torre);
+    //         break;
+    //     }else if (torre > TORRE_MAX){
+    //         printf("A torre nao pode andar %d casas\n", torre);
+    //     }else{
+    //         printf("valor invalido\n");
+    //     }
+	// }
+
+    // for(int i = 0; i < 1; i++){
+    //     int j = 0;
+    //     while(j < 1){
+    //         printf("O cavalo se movimentou em L para baixo e para a esquerda");
+    //         j++;
+    //     }
+    // }
 }
 
 int main(int argc, char** argcv){
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
 	
 	movimentacao();
 	return 0;
 }
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
